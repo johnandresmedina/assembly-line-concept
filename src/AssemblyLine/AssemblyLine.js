ecd0c3417da36e889864677287627e4652719982
John Medina <jhonandresmedina@gmail.com>
2022-02-24 19:07:43 -0500

    Adding task click handler to detect both left-click and right-click

--- src/AssemblyLine/AssemblyLine.js
+++ src/AssemblyLine/AssemblyLine.js
@@ -6,12 +6,26 @@ import AssemblyLineInput from './AssemblyLineInput';
 import Task from './Task';
 import { initializeState, reducer } from './reducer';
 
+const LEFT_CLICK_TYPE = 'click';
+const RIGHT_CLICK_TYPE = 'contextmenu';
+
 function AssemblyLine({ stages }) {
   const [state, dispatch] = useReducer(reducer, stages, initializeState);
   const { stagesLanes } = state;
 
   const handleInputEnter = itemName => {
     dispatch({ type: 'add-task', payload: itemName });
+  };
+
+  const handleTaskClick = event => {
+    event.preventDefault();
+
+    const eventType = event.type;
+    if (eventType === LEFT_CLICK_TYPE) {
+      console.log('Left click');
+    } else if (eventType === RIGHT_CLICK_TYPE) {
+      console.log('Right click');
+    }
   };
 
   return (
@@ -29,7 +43,9 @@ function AssemblyLine({ stages }) {
               <Stack spacing={2}>
                 {tasks.map(({ id, name }) => (
                   <Fragment key={id}>
-                    <Task>{name}</Task>
+                    <Task onClick={handleTaskClick} onContextMenu={handleTaskClick}>
+                      {name}
+                    </Task>
                   </Fragment>
                 ))}
               </Stack>
